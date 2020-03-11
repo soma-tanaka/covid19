db710beca219fc2e72e5de82499723cf9b1d499a
keitasuzukijp <hello@keitasuzuki.jp>
2020-03-11 22:23:07 +0900

    テキストを山梨版に変更

--- pages/parent.vue
+++ pages/parent.vue
@@ -20,15 +20,23 @@ export default {
         {
           title: '1 感染予防・健康管理',
           body:
-            '〇　不特定多数の人の集まる場所等への外出を避け、基本的に自宅で過ごしてください。<br />〇　手洗い、咳エチケット等により、感染予防に努めてください。<br />　<a href="https://tokyodouga.jp/lViN9C_BS-0.html" target="_blank" rel="noopener">【参考】感染症予防のための正しい手洗い方法（動画）</a> <br />〇　規則正しい生活を心がけ、日常の健康管理に十分気を付けてください。'
+            '〇  不特定多数の人の集まる場所等への外出を避け、基本的に自宅で過ごしてください。<br />〇  手洗い、咳エチケット等により、感染予防に努めてください。<br />　<a href="https://www.pref.yamanashi.jp/koucho/coronavirus/documents/tadasiitearai_1.pdf" target="_blank" rel="noopener">【参考】正しい手の洗い方（PDF：357KB）</a> <br />〇  規則正しい生活を心がけ、日常の健康管理に十分気を付けてください。<br />　<a href="https://www.pref.yamanashi.jp/koucho/coronavirus/documents/20200306newcoronabousi.pdf" target="_blank" rel="noopener">新型コロナウイルスを防ぐには（PDF：250KB）</a>'
         },
         {
           title: '2 感染症を疑う場合の対応',
           body:
-            '〇　風邪の症状や、37.5度以上の発熱が４日以上続いている、強いだるさ（倦怠感）、息苦しさ（呼吸困難）がある場合は、各保健所にご相談ください。<br />〇  「新型コロナウイルス感染症にかかる相談窓口について」（東京都福祉保健局）<br /><a href="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html" target="_blank" rel="noopener">https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html</a>'
+            '〇  風邪の症状や、37.5度以上の発熱が４日以上続いている、強いだるさ（倦怠感）、息苦しさ（呼吸困難）がある場合は、各保健所にご相談ください。<br /><a href="https://www.pref.yamanashi.jp/koucho/coronavirus/documents/20200306newcoronasentoutelfax.pdf" target="_blank" rel="noopener">〇  新型コロナウイルス感染症専用相談ダイヤル</a>'
         },
         {
-          title: '3 その他',
+          title: '3 学校の臨時休校について',
+          body: '〇  各市町村ごとの対応状況、学童保育実施についてお知らせしているページです。<br /><a href="https://www.pref.yamanashi.jp/kokusai/info_coronavirus/rinjikyuukou.html" target="_blank" rel="noopener">https://www.pref.yamanashi.jp/kokusai/info_coronavirus/rinjikyuukou.html</a>'
+        },
+        {
+          title: '4 子育て家庭休業助成金制度について',
+          body: '〇  子どもが小学校や保育所等へ登校・登園をしなかったことに伴い保護者が休業を余儀なくされた方に、一定額の助成を行うものです。<br /><a href="https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus.html#josei" target="_blank" rel="noopener">https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus.html#josei</a>'
+        },
+        {
+          title: '5 その他',
           body: '〇  詳細は、各学校からのお知らせ等をご確認ください。'
         }
       ]
